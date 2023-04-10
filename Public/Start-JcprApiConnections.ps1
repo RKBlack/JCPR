1f457d148dc22f88e6bb211fe697b8d14ee0aabe
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2023-04-10 14:27:26 -0500

    Added Windows Forms

--- Public/Start-JcprApiConnections.ps1
+++ Public/Start-JcprApiConnections.ps1
@@ -1,15 +1,15 @@
 function Start-JcprApiConnections {
     [CmdletBinding()]
     param (
-        [string]$HuduBaseUrl,
-        [string]$JcOrgId
+        
     )
 
     begin {
 
         $RequiredAssemblies = @(
             'System.Windows.Forms',
-            'System.Drawing'
+            'System.Drawing',
+            'PresentationFramework'
         )
         $RequiredAssemblies | ForEach-Object {
             if (-not (Get-Command $_ -ErrorAction SilentlyContinue)) {
@@ -32,25 +32,128 @@ function Start-JcprApiConnections {
 
     process {
         
+        $UpdateStoredCredentials = $false
+
+        $HuduBaseUrl = Get-StoredCredential -Target 'JCPR/HuduBaseUrl'
         $HuduApi = Get-StoredCredential -Target 'JCPR/HuduApi'
+        $JcOrgId = Get-StoredCredential -Target 'JCPR/JcOrgId'
         $JcApi = Get-StoredCredential -Target 'JCPR/JcApi'
 
-        if ($null -eq $HuduApi) {
-            $HuduCreds = Get-Credential -Message 'Enter your Hudu API Key'
-            New-StoredCredential -Target 'JCPR/HuduApi' -Credential $HuduCreds -Type Generic -Persist LocalMachine
-            $HuduApi = Get-StoredCredential -Target 'JCPR/HuduApi'
+        if ($HuduBaseUrl -and $HuduApi -and $JcOrgId -and $JcApi) {
+            Write-Verbose 'All API Connection Information is stored in the Credential Manager.'
+
+            $ReplaceMessage = 'Would  you like to replace the existing stored API connection information?'
+            $ReplaceTitle = 'Replace Stored Credentials?'
+            $ReplaceType = 'YesNo'
+            $ReplaceResult = [System.Windows.MessageBox]::Show($ReplaceMessage, $ReplaceTitle, $ReplaceType)
+
+            if ($ReplaceResult -eq 'Yes') {
+                Remove-StoredCredential -Target 'JCPR/HuduBaseUrl' -ErrorAction SilentlyContinue
+                Remove-StoredCredential -Target 'JCPR/HuduApi' -ErrorAction SilentlyContinue
+                Remove-StoredCredential -Target 'JCPR/JcOrgId' -ErrorAction SilentlyContinue
+                Remove-StoredCredential -Target 'JCPR/JcApi' -ErrorAction SilentlyContinue
+                $UpdateStoredCredentials = $true
+            }
         }
-        if ($null -eq $JcApi) {
-            $JcCreds = Get-Credential -Message 'Enter your JumpCloud API Key'
-            New-StoredCredential -Target 'JCPR/JcApi' -Credential $JcCreds -Type Generic -Persist LocalMachine
+        
+        if (-not $HuduBaseUrl -or -not $HuduApi -or -not $JcOrgId -or -not $JcApi -or $UpdateStoredCredentials) {
+            $ApiForm = New-Object Windows.Forms.Form
+            $ApiForm.Text = 'Enter your API Connection Information'
+            $ApiForm.Size = New-Object System.Drawing.Size(400, 320)
+            $ApiForm.StartPosition = 'CenterScreen'
+
+            $HuduBaseUrlLabel = New-Object System.Windows.Forms.Label
+            $HuduBaseUrlLabel.Location = New-Object System.Drawing.Point(10, 10)
+            $HuduBaseUrlLabel.Size = New-Object System.Drawing.Size(340, 20)
+            $HuduBaseUrlLabel.Text = 'Enter the Hudu BaseUrl:'
+            $ApiForm.Controls.Add($HuduBaseUrlLabel)
+
+            $HuduBaseUrlForm = New-Object Windows.Forms.TextBox
+            $HuduBaseUrlForm.Location = New-Object System.Drawing.Point(10, 30)
+            $HuduBaseUrlForm.Size = New-Object System.Drawing.Size(340, 20)
+            $HuduBaseUrlForm.Height = 80
+            $ApiForm.Controls.Add($HuduBaseUrlForm)        
+            
+            $HuduApiLabel = New-Object System.Windows.Forms.Label
+            $HuduApiLabel.Location = New-Object System.Drawing.Point(10, 60)
+            $HuduApiLabel.Size = New-Object System.Drawing.Size(340, 20)
+            $HuduApiLabel.Text = 'Enter the Hudu API Key:'
+            $ApiForm.Controls.Add($HuduApiLabel)
+
+            $HuduApiForm = New-Object Windows.Forms.MaskedTextBox
+            $HuduApiForm.PasswordChar = '*'
+            $HuduApiForm.Location = New-Object System.Drawing.Point(10, 80)
+            $HuduApiForm.Size = New-Object System.Drawing.Size(340, 20)
+            $HuduApiForm.Height = 80
+            $ApiForm.Controls.Add($HuduApiForm)        
+
+            $JcOrgIdLabel = New-Object System.Windows.Forms.Label
+            $JcOrgIdLabel.Location = New-Object System.Drawing.Point(10, 110)
+            $JcOrgIdLabel.Size = New-Object System.Drawing.Size(340, 20)
+            $JcOrgIdLabel.Text = 'Enter the JumpCloud Org Id:'
+            $ApiForm.Controls.Add($JcOrgIdLabel)
+         
+            $JcOrgIdForm = New-Object Windows.Forms.TextBox
+            $JcOrgIdForm.Location = New-Object System.Drawing.Point(10, 130)
+            $JcOrgIdForm.Size = New-Object System.Drawing.Size(340, 20)
+            $JcOrgIdForm.Height = 80
+            $ApiForm.Controls.Add($JcOrgIdForm)
+
+            $JcApiLabel = New-Object System.Windows.Forms.Label
+            $JcApiLabel.Location = New-Object System.Drawing.Point(10, 160)
+            $JcApiLabel.Size = New-Object System.Drawing.Size(340, 20)
+            $JcApiLabel.Text = 'Enter the JumpCloud API Key:'
+            $ApiForm.Controls.Add($JcApiLabel)
+         
+            $JcApiForm = New-Object Windows.Forms.MaskedTextBox
+            $JcApiForm.PasswordChar = '*'
+            $JcApiForm.Location = New-Object System.Drawing.Point(10, 180)
+            $JcApiForm.Size = New-Object System.Drawing.Size(340, 20)
+            $JcApiForm.Height = 80
+            $ApiForm.Controls.Add($JcApiForm)        
+        
+            $ApiFormOkButton = New-Object System.Windows.Forms.Button
+            $ApiFormOkButton.Location = New-Object System.Drawing.Point(115, 240)
+            $ApiFormOkButton.Size = New-Object System.Drawing.Size(75, 23)
+            $ApiFormOkButton.Text = 'OK'
+            $ApiFormOkButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+            $ApiForm.AcceptButton = $ApiFormOkButton
+            $ApiForm.Controls.Add($ApiFormOkButton)
+
+            $ApiFormCancelButton = New-Object System.Windows.Forms.Button
+            $ApiFormCancelButton.Location = New-Object System.Drawing.Point(210, 240)
+            $ApiFormCancelButton.Size = New-Object System.Drawing.Size(75, 23)
+            $ApiFormCancelButton.Text = 'Cancel'
+            $ApiFormCancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+            $ApiForm.ApiFormCancelButton = $ApiFormCancelButton
+            $ApiForm.Controls.Add($ApiFormCancelButton)
+
+            $ApiForm.ShowDialog()
+        
+            New-StoredCredential -Target 'JCPR/HuduBaseUrl' -UserName 'HuduBaseUrl' -Password $HuduBaseUrlForm.Text -Type Generic -Persist LocalMachine
+            $HuduApi = Get-StoredCredential -Target 'JCPR/HuduBaseUrl'
+
+            New-StoredCredential -Target 'JCPR/HuduApi' -UserName 'HuduAPI' -Password $HuduApiForm.Text -Type Generic -Persist LocalMachine
+            $HuduApi = Get-StoredCredential -Target 'JCPR/HuduApi'
+
+            New-StoredCredential -Target 'JCPR/JcOrgId' -UserName 'JcOrgId' -Password $JcOrgIdForm.Text -Type Generic -Persist LocalMachine
+            $JcOrgId = Get-StoredCredential -Target 'JCPR/JcOrgId'
+
+            New-StoredCredential -Target 'JCPR/JcApi' -UserName 'JcAPI' -Password $JcApiForm.Text -Type Generic -Persist LocalMachine
             $JcApi = Get-StoredCredential -Target 'JCPR/JcApi'
+
         }
 
         New-HuduAPIKey -ApiKey (ConvertFrom-SecureString (Get-StoredCredential -Target 'JCPR/HuduApi').Password -AsPlainText)
-        New-HuduBaseURL -BaseURL $HuduBaseUrl
-        Connect-JCOnline -JumpCloudAPIKey (ConvertFrom-SecureString (Get-StoredCredential -Target 'JCPR/JcApi').Password -AsPlainText) -JumpCloudOrgID $JcOrgId
+        New-HuduBaseURL -BaseURL (ConvertFrom-SecureString (Get-StoredCredential -Target 'JCPR/HuduBaseUrl').Password -AsPlainText)
+        $JCOnlineParams = @{
+            JumpCloudAPIKey = (ConvertFrom-SecureString (Get-StoredCredential -Target 'JCPR/JcApi').Password -AsPlainText)
+            JumpCloudOrgID = (ConvertFrom-SecureString (Get-StoredCredential -Target 'JCPR/JcOrgId').Password -AsPlainText)
+        }
+        Connect-JCOnline $JCOnlineParams
 
     }
+    
     end {
 
     }
